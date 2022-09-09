630f838cfb3a660553c8c0fc640072b34ccbb9de
namy <namy_park@protonmail.com>
2022-09-10 00:36:59 +0100

    Added comments to explain the add cart function in carts

--- carts/views.py
+++ carts/views.py
@@ -14,21 +14,29 @@ def _cart_id(request):
 
 
 def add_cart(request, product_id):
-    # getting the product here
+    # getting the product here using the product id
     product = Product.objects.get(id=product_id)
     # getting the product variation here
     product_variation = []
     variation = ""
+    # if the request is a post request we loop the item(s) and assign them the variable KEY
     if request.method == "POST":
         for item in request.POST:
             key = item
+         # we get the value for the responding key elements(as in a dict)
+         # ex: if the item is 'color' it is assigned to the key variable 
+         #then the value for the key is provided hence 'red,blue' etc
             value = request.POST.get(key)
+             
+            # WE get the variation objects by filtering by the product we got
+            # and the variation category (key) and the value(value)
             try:
                 variation = Variation.objects.get(
                     product=product,
                     variation_category__iexact=key,
                     variation_value__iexact=value,
                 )
+                #we append the variation inside the list ie [product,color(blue),size(medium)]
                 product_variation.append(variation)
 
             except:
@@ -42,10 +50,14 @@ def add_cart(request, product_id):
 
     except Cart.DoesNotExist:
         cart = Cart.objects.create(cart_id=_cart_id(request))
+        #if the cart does not exist we create the cart
 
     cart.save()
     # GETTING THE CART ITEM HERE
+    # here we filter if the product is already in the cart(this returns a BOOL)
     is_cart_item_exist = CartItem.objects.filter(product=product, cart=cart).exists()
+    # if it is we filter the cartItem, we make a empty list to hold the existing variation and ID 
+    
     if is_cart_item_exist:
         cart_item = CartItem.objects.filter(product=product, cart=cart)
 
@@ -55,22 +67,27 @@ def add_cart(request, product_id):
 
         existing_variation_list = []
         id = []
+        # we loop through the cart item and for each item we get their variation
+        # AND append that to the existing variation list and append the ID to the ID list
+        
         for item in cart_item:
             existing_variations = item.variations.all()
             existing_variation_list.append(list(existing_variations))
             id.append(item.id)
-        print(existing_variations)
+            
+        # here we check for existing variation index in the product variation
         if product_variation in existing_variation_list:
             # increase the cartItem quantity
             index = existing_variation_list.index(product_variation)
-            print(index)
+            #we get the index and the index of the item_id
             item_id = id[index]
-            print(item_id)
             item = CartItem.objects.get(product=product, id=item_id)
-            print(item)
+            # we get the item from the item cartItem(with the product and item_id) and increase the quantity
+            
             item.quantity += 1
             item.save()
         else:
+            #if the existing variation is not in product_variation we create the item
             # create a new item
             item = CartItem.objects.create(product=product, quantity=1, cart=cart)
             if len(product_variation) > 0:
@@ -78,6 +95,7 @@ def add_cart(request, product_id):
                 item.variations.add(*product_variation)
             item.save()
     else:
+        # we create the product if it does not exist and add the variations to it 
         cart_item = CartItem.objects.create(product=product, quantity=1, cart=cart)
 
         if len(product_variation) > 0:
